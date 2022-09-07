2b083dd59a9ab48532dc5a4b952b58504ec394f7
Kevin Murphy <murphy.kevin.mail@gmail.com>
2022-09-06 22:01:34 -0400

    introduce solid state amp

---
+++ lib/blues/solid_state_amplifier.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Blues
+  class SolidStateAmplifier < Amplifier
+    def preamp_tone
+      "🫙"
+    end
+  end
+end
---
+++ spec/blues/solid_state_amplifier_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Blues
+  RSpec.describe TubeAmplifier do
+    describe "#amplify" do
+      it "provides the sound value at the volume specified" do
+        amp = SolidStateAmplifier.new(volume: 3)
+        amp.turn_on
+
+        note = Note.new(root_note: :c, root_octave: 1, offset: 0)
+
+        expect(amp.amplify(note)).to eq "C1🫙🔈[3]"
+      end
+
+      it "produces no sound when the amplifier isn't turned on" do
+        amp = SolidStateAmplifier.new(volume: 3)
+        note = Note.new(root_note: :c, root_octave: 1, offset: 0)
+
+        expect(amp.amplify(note)).to eq "🔇[3]"
+      end
+
+      it "does nothing when the amplifier is on but with no volume" do
+        amp = SolidStateAmplifier.new(volume: 0)
+        amp.turn_on
+
+        note = Note.new(root_note: :c, root_octave: 1, offset: 0)
+
+        expect(amp.amplify(note)).to eq "🔇[0]"
+      end
+    end
+  end
+end
